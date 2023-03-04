76381bcbedef190bfcf8cd75aab7f0c25d7dd52c
Aks32216 <amishsingh568@gmail.com>
2023-03-05 02:25:54 +0530

    done backend

--- controller/imageController.js
+++ controller/imageController.js
@@ -7,11 +7,13 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage=async (req,res)=>{
+    const {prompt,size}=req.body;
+    const imgSize=size==='small'?'256x256':size==='medium'?'512x512':'1024x1024';
     try {
         const response = await openai.createImage({
-            prompt: "a white siamese cat",
+            prompt: prompt,
             n: 1,
-            size: "1024x1024",
+            size: imgSize
         });
         const image_url = response.data.data[0].url;
         res.status(200).json({
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express=require("express");
+const path=require("path");
 const dotenv=require("dotenv").config();
 const app=express();
 const openaiRoute=require("./routes/openaiRoutes.js")
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
 app.use('/openai',openaiRoute);
+
 
 const port=process.env.PORT || 5000;
 const key=process.env.OPENAI_API_KEY;
